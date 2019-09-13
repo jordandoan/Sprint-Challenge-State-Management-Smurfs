d5d313f7672353d2c4294abad535d1ed2e2ef041
Jordan <jordandoan@hotmail.com>
2019-09-13 09:06:54 -0700

    created form component

--- smurfs/src/components/Form.js
+++ smurfs/src/components/Form.js
@@ -11,7 +11,13 @@ const Form = () => {
     <div>
       <form>
         <h2>CREATE YOUR SMURF!</h2>
-        <input type="text"></input>
+        <p>Name</p>
+        <input type="text" placeholder="name" value={name} onChange={(e) => handleName(e)}/>
+        <p>Age</p>
+        <input type="text" placeholder="age" value={age} onChange={(e) => handleAge(e)}/>
+        <p>Height</p>
+        <input type="text" placeholder="height" value={height} onChange={(e) => handleHeight(e)}/>
+        <button>Add Smurf</button>
       </form>
     </div>
   )
--- smurfs/src/components/actions/index.js
+++ smurfs/src/components/actions/index.js
@@ -4,10 +4,15 @@ export const FETCHING = "FETCHING";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAILURE = "FETCH_FAILURE";
 
+const URL = "http://localhost:3333/smurfs";
+
 export const fetchData = () => dispatch => {
-  const URL = "http://localhost:3333/smurfs";
   dispatch({type: FETCHING})
   axios.get(URL)
     .then(res => dispatch({type: FETCH_SUCCESS, payload: res.data}))
     .catch(err => dispatch({type: FETCH_FAILURE, payload: err.response}));
+}
+
+export const addSmurf = (smurf) => dispatch => {
+  axios.post()
 }
